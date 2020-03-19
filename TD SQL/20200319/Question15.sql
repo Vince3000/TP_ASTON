3b1184a97c5733c9c19fb4443b950978c24daec8
Vince3000 <61778304+Vince3000@users.noreply.github.com>
2020-03-19 10:35:52 +0100

    Question14&Question15

---
+++ TD SQL/20200319/Question15.sql
@@ -0,0 +1,8 @@
+/*---------------------------Question15-----------------------------*/
+/*15. Lister les bières du continent ‘Afrique’*/
+
+select a.NOM_ARTICLE FROM beer.article a
+INNER JOIN beer.marque m on a.ID_MARQUE=m.ID_MARQUE
+INNER JOIN beer.pays p ON m.ID_PAYS=p.ID_PAYS
+INNER JOIN beer.continent c on p.ID_CONTINENT=c.ID_CONTINENT
+where c.NOM_CONTINENT LIKE ('%afrique%');
