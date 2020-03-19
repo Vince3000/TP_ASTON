a1b128c49b8be47284ab981dbb7e4ecdde2095f6
Vince3000 <61778304+Vince3000@users.noreply.github.com>
2020-03-19 09:28:07 +0100

    Question1

---
+++ TD SQL/20200319/Question1.sql
@@ -0,0 +1,7 @@
+/*--------------------------Question1-------------------------------------*/
+
+SELECT t.NUMERO_TICKET FROM beer.ticket t
+INNER JOIN beer.ventes v on t.NUMERO_TICKET=v.NUMERO_TICKET
+INNER JOIN beer.article A on v.ID_ARTICLE=A.ID_ARTICLE
+where a.ID_ARTICLE=500;
+
